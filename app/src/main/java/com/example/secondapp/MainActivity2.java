65ebff588ae63f330728553ef6b54095da951ce1
mahi <127742360+mahi1974@users.noreply.github.com>
2024-12-16 10:53:19 +0530

    first

---
+++ app/src/main/java/com/example/secondapp/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.secondapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView lbl= findViewById(R.id.textView);
+        // In the second activity (Activity2)
+        Intent a = getIntent();
+        lbl.setText(a.getStringExtra("Second"));
+
+    }
+}
